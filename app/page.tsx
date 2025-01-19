9c2cc9f6d886c04e704c4623475ab0a20d154af8
James <thejamescodes@gmail.com>
2025-01-19 22:27:23 +0800

    Fixed landing page

--- app/page.tsx
+++ app/page.tsx
@@ -9,12 +9,12 @@ export default async function Home() {
   return (
     
       <main className="max-w-4xl mx-auto mt-4">
-           <LoginForm/>
-          {/* <div className="text-center my-5 flex flex-col gap-4">
+           {/* <LoginForm/> */}
+          <div className="text-center my-5 flex flex-col gap-4">
             <h1 className="text-2xl font-bold"> Todo List App</h1>
             <AddTask/>
           </div>
-          <TodoList tasks={tasks}/> */}
+          <TodoList tasks={tasks}/>
       </main>
      
 
